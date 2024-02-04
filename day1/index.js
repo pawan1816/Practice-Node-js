277efe6f6329e19755dc40d2a8ded4f14908b376
unknown <paikpawan18@gmail.com>
2024-02-04 20:22:52 +0530

    added first day simple coding in node js

---
+++ day1/index.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+var x=20;
+var y=32;
+var z=232;
+console.warn(x+y+z);
---
+++ day1/p.js
@@ -0,0 +1,5 @@
+console.log("my name is pawan Kumar ");
+var x=20;
+var y=32;
+var z=232;
+console.warn(x+y+z);
